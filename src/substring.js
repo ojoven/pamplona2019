ef918987bf8f85af33683e16a7685c65fdc33000
ojoven <mikeltorresmail@gmail.com>
2019-03-30 13:09:28 +0100

    Multiplies OK

--- src/substring.js
+++ src/substring.js
@@ -3,7 +3,7 @@ export default class Substring {
   substring(string1, string2, indexStart, indexEnd) {
 
     let substring = ''
-    substring = substring.slice(0, indexEnd)
+    substring = string1.slice(0, indexEnd)
     return substring
   }
 }
