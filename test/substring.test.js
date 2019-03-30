8c6e4d2ece82b600f97499c79836995f978a2d09
ojoven <mikeltorresmail@gmail.com>
2019-03-30 11:34:25 +0100

    Multiplies OK

--- test/substring.test.js
+++ test/substring.test.js
@@ -5,4 +5,10 @@ describe('Substring', () => {
     expect(true).toBeTruthy()
   })
 
+  it('2 empty strings return empty string', () => {
+    let substring = new Substring;
+    let result = substring.substring('', '', 0, 0);
+    expect(result).toBe('')
+  })
+
 })
