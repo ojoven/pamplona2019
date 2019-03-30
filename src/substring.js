5fbf75436dfc2a87e3058d2e6bafb8d1ca3e18d0
ojoven <mikeltorresmail@gmail.com>
2019-03-30 12:59:42 +0100

    Multiplies OK

--- src/substring.js
+++ src/substring.js
@@ -1,9 +1,7 @@
 export default class Substring {
 
   substring(string1, string2, indexStart, indexEnd) {
-    if (string1 === '' && string2 === '') {
-      return ''
-    }
+
     let substring = ''
     for (let i = 0; i < indexEnd; i++) {
       substring += string1[i];
